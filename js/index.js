ac0b9bf63b37e640e365d9fa47b5fe5efa17d242
DarkSuN <supanatkan2542@gmail.com>
2020-04-25 03:16:43 +0700

    update not fin

--- js/index.js
+++ js/index.js
@@ -5,6 +5,12 @@ $(function () {
     $.getJSON(url, function (result) {
 
         var no = 1;
+        var con = 0;
+        var death = 0;
+        var reco =0;
+        var con1 = 0;
+        var death1 = 0;
+        var reco1 =0;
         for (var country in result) {
           var selectedCountry = result[country];
           console.log(selectedCountry);
@@ -17,19 +23,43 @@ $(function () {
                     </tr>`;
 
             $("#data").append(row);
+            con=con+selectedCountry[selectedCountry.length-1].confirmed;
+            death=death+selectedCountry[selectedCountry.length-1].deaths;
+            reco=reco+selectedCountry[selectedCountry.length-1].recovered;
+            con1=con1+(selectedCountry[selectedCountry.length-1].confirmed-selectedCountry[selectedCountry.length-2].confirmed);
+            death1=death1+(selectedCountry[selectedCountry.length-1].deaths-selectedCountry[selectedCountry.length-2].deaths);
+            reco1=reco1+(selectedCountry[selectedCountry.length-1].recovered-selectedCountry[selectedCountry.length-2].recovered);
+            
             no++;
-          if(no==selectedCountry.length){
-           var row1 = `<div >${selectedCountry[selectedCountry.length-1].confirmed}</div>`
+          if(no==selectedCountry.length+1){
+
+            
+
+           var row1 = `<div >${con}</div>`
 
            $("#confirmed").append(row1);
 
-            var row2 = `<div >${selectedCountry[selectedCountry.length-1].deaths}</div>`
+            var row2 = `<div >${death}</div>`
 
             $("#death").append(row2);
 
-            var row3 = `<div >${selectedCountry[selectedCountry.length-1].recovered}</div>`
+            var row3 = `<div >${reco}</div>`
 
            $("#recovered").append(row3);
+
+           var row11 = `<div >${con1}</div>`
+
+           $("#confirmed1").append(row11);
+
+            var row22 = `<div >${death1}</div>`
+
+            $("#death1").append(row22);
+
+            var row33 = `<div >${reco1}</div>`
+
+           $("#recovered1").append(row33);
+
+
           }
 
         }
